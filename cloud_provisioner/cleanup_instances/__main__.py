e3fa2a844e8aa0ea7b051dc20a537b9a801206a2
Chenxing Li <ChenxingLi@users.noreply.github.com>
2026-04-26 10:28:50 +0800

    Add log-path option and skip bombed Bahrain region
    - Add --log-path argument to create_instances for writing provision
      logs to a specified directory
    - Pass log path from one_click.sh to create_instances
    - Disable me-south-1 (Bahrain) region due to unavailability
    - Propagate exceptions from cleanup futures via future.result()

--- cloud_provisioner/cleanup_instances/__main__.py
+++ cloud_provisioner/cleanup_instances/__main__.py
@@ -28,7 +28,7 @@ AWS_REGIONS = [
     "ap-east-1",   # Hong Kong
     "sa-east-1",   # São Paulo
     "af-south-1",  # Cape Town
-    "me-south-1",  # Bahrain
+    # "me-south-1",  # Bahrain (Bombed)
 ]
 TENCENT_REGIONS = [
     "ap-hongkong",
@@ -94,7 +94,8 @@ if __name__ == "__main__":
             executor.submit(delete_instances, tencent_client, TENCENT_REGIONS, predicate=predicate),
         ]
         from concurrent.futures import wait
-
-        wait(futures)
+        
+        for future in futures:
+            future.result()
         
         
